f9d2126f8fa366b4a923b55f171f60b5aa9a16e4
Vanessa <v.d.chacon@gmail.com>
2022-02-11 12:03:47 -0800

    update path

--- Capstone/issues_environment/management/commands/loaddata.py
+++ Capstone/issues_environment/management/commands/loaddata.py
@@ -7,9 +7,9 @@ from issues_environment.models import Countries, Issues
 
 class Command(BaseCommand):
     def handle(self,*args,**options):
-        factbook_path = '../../factbook.json/'
+        # factbook_path = '../../factbook.json/'
         # for country in countries:
-        us_data_filepath = factbook_path + 'north-america/us.json'
+        us_data_filepath = 'data/north-america/us.json'
         with open(us_data_filepath, 'r', encoding='utf-8') as f:
             us_data = json.load(f)   
         e_data=(us_data['Environment']['Environment - current issues']['text'])
@@ -18,19 +18,3 @@ class Command(BaseCommand):
         for issue in env_data:
             print(issue)
 
-# for issue in data.split('; '):
-#     issue = issue(text)
-#     print(type(issue))
-#     print()
-
-# class Country:
-#     def_init_(self,name, issues):
-#     self.name = name
-#     self.issues = data.split(';')
-# need to import json somewhere (views?)
-# data= json.loads() ---- ??? environmental issues or whatever the name of it is in json right?
-# print(data)
-# with open ('country.json') as f:
-    # data = json.load(f) (name guessed at this point)
-# custom management
-#
