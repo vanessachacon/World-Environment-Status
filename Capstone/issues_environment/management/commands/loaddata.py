803a930d511258527e26598f9c9cfab8491704bd
Vanessa <v.d.chacon@gmail.com>
2022-02-23 10:20:08 -0800

    command management wip

--- Capstone/issues_environment/management/commands/loaddata.py
+++ Capstone/issues_environment/management/commands/loaddata.py
@@ -2,34 +2,36 @@
 from django.core.management.base import BaseCommand
 import json
 from issues_environment.models import Country, Issue
-
+import pandas as pd
+import os
 
 class Command(BaseCommand):
-    def handle(self,*args,**options):
+    def handle(self, *args, **options):
+        entries = os.listdir('data/north-america')
+        for entry in entries:
+        # print(entry)
+            with open(f'data/north-america/{entry}', 'r', encoding='utf-8') as f:
+                all_na_data = (json.load(f))
+                country_names = (all_na_data['Government']['Country name'] ['conventional short form']['text'])
+                print(country_names)
+                country, _created = Country.objects.get_or_create(name=country_names)
+                print(country)
+            if _created:
+                self.stdout.write(f'{country_names} created')
+            country.issues.add()
+            country.save()
         us_data_filepath = 'data/north-america/us.json'
         with open(us_data_filepath, 'r', encoding='utf-8') as f:
-            us_data = json.load(f)   
-
-
-        country_names=(us_data['Government']['Country name']['conventional short form']['text'])
-
-        country, _created = Country.objects.get_or_create(name = country_names)
-        print(country)
-        if _created:
-            self.stdout.write(f'{country_names} created')
-        country.issues.add()
-        country.save()
-        us_data_filepath = 'data/north-america/us.json'
-        with open(us_data_filepath, 'r', encoding='utf-8') as f:
-            us_data = json.load(f)   
-        e_data=(us_data['Environment']['Environment - current issues']['text'])
+            us_data = json.load(f)
+        e_data = (us_data['Environment']
+                  ['Environment - current issues']['text'])
         env_data = (e_data.split('; '))
         for issue_text in env_data:
-            issue, _created = Issue.objects.get_or_create(text = issue_text)
+            issue, _created = Issue.objects.get_or_create(text=issue_text)
             print(issue)
             if _created:
                 self.stdout.write(f'{issue} created')
-            country.issues.add(issue) 
+            country.issues.add(issue)
 
 #   bd_data = json.load(f)
 #             ca_data = json.load(f)
--- Capstone/issues_environment/management/commands/savedcode.py
+++ Capstone/issues_environment/management/commands/savedcode.py
@@ -1,39 +1,39 @@
 
-import pandas as pd
 from django.core.management.base import BaseCommand
 import json
 from issues_environment.models import Country, Issue
+import pandas as pd
 import os
 
-base_dir = 'data/north-america'
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        entries = os.listdir('data/north-america')
+        for entry in entries:
+            with open(f'data/north-america/{entry}', 'r', encoding='utf-8') as f:
+                all_na_data = (json.load(f))
+                country_names = (all_na_data['Government']['Country name'] ['conventional short form']['text'])
+                country, _created = Country.objects.get_or_create(name=country_names)
+                print(country)
+            if _created:
+                self.stdout.write(f'{country_names} created')
+            country.issues.add()
+            country.save()
 
-data_list = []
-for file in os.listdir(base_dir):
+        for entry in entries:
+            with open(f'data/north-america/{entry}', 'r', encoding='utf-8') as f:
+                all_na_data = (json.load(f))
+            e_data = (all_na_data['Environment']['Environment - current issues']['text'])
+            env_data = (e_data.split('; '))
+        for issue_text in env_data:
+            issue, _created = Issue.objects.get_or_create(text=issue_text)
+            print(issue)
+            if _created:
+                self.stdout.write(f'{issue} created')
+            country.issues.add(issue)
 
-    # If file is a json, construct it's full path and open it, append all json data to list
-    if 'json' in file:
-        json_path = os.path.join(base_dir, file)
-        json_data = pd.read_json(json_path, lines=True)
-        data_list.append(json_data)
-print(data_list)
-
-# country_names=(us_data['Government']['Country name']['conventional short form']['text'])
-# country, _created = Country.objects.get_or_create(name = country_names)
-# print(country)
-# if _created:
-#     self.stdout.write(f'{country_names} created')
-# country.issues.add()
-# country.save()
-
-# us_data_filepath = 'data/north-america/us.json'
-# with open(us_data_filepath, 'r', encoding='utf-8') as f:
-#     us_data = json.load(f)
-# e_data=(us_data['Environment']['Environment - current issues']['text'])
-# env_data = (e_data.split('; '))
-
-# for issue_text in env_data:
-#     issue, _created = Issue.objects.get_or_create(text = issue_text)
-#     print(issue)
-#     if _created:
-#         self.stdout.write(f'{issue} created')
-#     country.issues.add(issue)
+#   bd_data = json.load(f)
+#             ca_data = json.load(f)
+#             gl_data = json.load(f)
+#             ip_data = json.load(f)
+#             mx_data = json.load(f)
+#             sb_data = json.load(f)
