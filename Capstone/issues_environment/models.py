444906b42f60974476ed4ea02063054680cf2a0f
Vanessa <v.d.chacon@gmail.com>
2022-02-25 12:34:11 -0800

    heroku stuffff

--- Capstone/issues_environment/models.py
+++ Capstone/issues_environment/models.py
@@ -5,7 +5,7 @@ class Country(models.Model):
     class Meta: 
         verbose_name_plural = 'Countries'
 
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=500)
     issues = models.ManyToManyField('Issue', related_name='countries') # related_name= keyword argument overrides the default
     # the default way to get all countries related to an issue would be issue.country_set.all()
     # with related_name='countries', you can just do issue.countries.all()
@@ -16,7 +16,7 @@ class Country(models.Model):
 
 
 class Issue(models.Model):
-    text = models.CharField(max_length=50)
+    text = models.CharField(max_length=500)
 
 
     def __str__(self):
